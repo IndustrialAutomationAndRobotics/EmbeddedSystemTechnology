f4342107168585342f9d603906f6b7d762adbf59
hazimgharib21 <hazimgharib21@gmail.com>
2018-04-01 22:31:08 +0800

    updated

---
+++ arduino2arduino/slavei2c/slavei2c.ino
@@ -0,0 +1,27 @@
+#include <Wire.h>
+
+bool state = false;
+
+void setup() {
+  Wire.begin(8);
+  Wire.onReceive(receiveEvent);
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  
+}
+
+void loop() {
+  delay(100);
+  digitalWrite(13,state);
+}
+
+void receiveEvent(int howMany){
+  while(1 < Wire.available()){
+    char c = Wire.read();
+    Serial.print(c);
+  }
+  int x = Wire.read();
+  Serial.println(x);
+  state = !state;
+}
+
